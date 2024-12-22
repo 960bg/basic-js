82da6b9039d7ff0bb9ebf516c25236e7235d0996
960bg <74264055+960bg@users.noreply.github.com>
2024-12-22 19:37:38 +0400

    feat: [encodeLine]: done. [Sun, Dec 22, 2024 07:37:37 PM]

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require( '../extensions/index.js' );
 
 /**
  * Given a string, return its encoding version.
@@ -10,10 +10,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine( str ) {
+  if ( str.length <= 1 ) return str;
+
+  const mas = str.split( '' );
+
+  let count = 1;
+  let resStr = '';
+
+  for ( let i = 0; i < mas.length; i++ ) {
+    // если последний элемент то проверить с предыдущим
+    if ( i === mas.length - 1 ) {
+      if ( mas[i - 1] === mas[i] ) {
+        // count += 1;
+        resStr += count + mas[i];
+        return resStr;
+      }
+      resStr += mas[i];
+      return resStr;
+    }
+
+    if ( mas[i] === mas[i + 1] ) {
+      count += 1;
+
+    } else {
+
+      // были предыдущие совпадения то вставить их кол-во и букву, а потом текущую букву
+      if ( count !== 1 ) {
+        resStr += count + mas[i - 1];
+        count = 1;
+      } else {
+        // совпадений не было
+        resStr += mas[i];
+      }
+
+    }
+
+  }
 }
+
+
 
 module.exports = {
   encodeLine
