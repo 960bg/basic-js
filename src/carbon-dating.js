11c1bbdeb9652d9b06904ed37c0437833efe994c
960bg <74264055+960bg@users.noreply.github.com>
2024-12-22 15:54:19 +0400

    feat: [getDNSStats]: done. [Sun, Dec 22, 2024 03:54:19 PM]

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require( '../extensions/index.js' );
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -17,44 +17,32 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(sampleActivity = '') {
+function dateSample( sampleActivity = '' ) {
 
-  if (typeof (sampleActivity) !== "string") {
+  if ( typeof ( sampleActivity ) !== "string" ) {
     return false;
   }
 
   sampleActivity = +sampleActivity;
-  if (typeof (+sampleActivity) !== "number") {
+  if ( typeof ( +sampleActivity ) !== "number" ) {
     return false;
   }
 
-
-  if (+sampleActivity == 0) {
+  if ( +sampleActivity == 0 ) {
     return false;
   }
 
-  if ((+sampleActivity <= 0) || (+sampleActivity > 15) || (Number.isNaN(sampleActivity))) {
+  if ( ( +sampleActivity <= 0 ) || ( +sampleActivity > 15 ) || ( Number.isNaN( sampleActivity ) ) ) {
     return false;
   }
-
-
-  // const MODERN_ACTIVITY = 15;
-  // const HALF_LIFE_PERIOD = 5730;
-  // throw new NotImplementedError('Not implemented');
-  // // remove line with error and write your code here
 
   let k = 0.693 / HALF_LIFE_PERIOD;
-  let activity = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
+  let activity = Math.ceil( Math.log( MODERN_ACTIVITY / sampleActivity ) / k );
   return activity;
-
-
 }
 
 module.exports = {
   dateSample
 };
 
-
-// dateSample('3');
-// dateSample(' \n\t\r');
-dateSample('ACTIVITY OVER 9000');
+dateSample( 'ACTIVITY OVER 9000' );
--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require( '../extensions/index.js' );
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,9 +22,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats( domains ) {
+  const obj = {};
+
+  for ( const domain of domains ) {
+    getDomain( domain );
+  }
+
+  return obj;
+
+  function getDomain( str ) {
+    const mas = str.split( '.' ).map( v => `.${ v }` );
+    const resMas = [];
+    let indexResMas = 0;
+    for ( let i = mas.length - 1; i >= 0; i-- ) {
+      if ( resMas.length === 0 ) {
+        resMas.push( mas[i] );
+        addProp( mas[i] );
+      } else {
+        resMas.push( `${ resMas[indexResMas] + mas[i] }` );
+        addProp( `${ resMas[indexResMas] + mas[i] }` );
+        indexResMas += 1;
+      }
+    }
+  }
+
+  function addProp( prop ) {
+    if ( Object.hasOwn( obj, prop ) ) {
+      obj[prop] += 1;
+      return;
+    }
+    obj[prop] = 1;
+    return;
+  }
+
 }
 
 module.exports = {
