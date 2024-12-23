1f19b2c185244b576ad00f5b9176888c8ec3e5df
960bg <74264055+960bg@users.noreply.github.com>
2024-12-23 12:37:57 +0400

    feat: [mac-address]: done [Mon, Dec 23, 2024 12:37:57 PM]

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require( '../extensions/index.js' );
 
 /**
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
@@ -14,9 +14,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address( strAddr ) {
+  const makAddr = strAddr.split( '-' );
+  let res = 0;
+  if ( makAddr.length > 6 ) {
+    res = false;
+    console.debug( 'strAddr', strAddr );
+    console.debug( 'res', res );
+    return res;
+  }
+  for ( const el of makAddr ) {
+    if ( isMak( el ) === false ) {
+      res = false;
+      console.debug( 'strAddr', strAddr );
+      console.debug( 'el', el );
+      console.debug( 'res', res );
+      return res;
+    }
+  }
+
+  res = true;
+  console.debug( 'strAddr', strAddr );
+  console.debug( 'res', res );
+  return res;
+
+  function isMak( addr ) {
+    const regexp = /[a-f\d]/i;
+    const masMak = addr.split( '' );
+    for ( const el of masMak ) {
+      if ( el.search( regexp ) === -1 ) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 module.exports = {
   isMAC48Address
